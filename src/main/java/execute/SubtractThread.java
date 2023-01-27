033a298f86db087b0e9cbee3ab2495047d82e153
Ivanildo Junior <ivanildo@outlook.com>
2023-01-27 17:32:21 -0300

    Create subtract thread

---
+++ src/main/java/execute/SubtractThread.java
@@ -0,0 +1,15 @@
+package execute;
+
+public class SubtractThread implements Runnable {
+    Integer v1, v2;
+
+    public SubtractThread(Integer v1, Integer v2){
+        this.v1 = v1;
+        this.v2 = v2;
+    }
+    @Override
+    public void run() {
+        System.out.println("Sum Thread: " + (v1 - v2));
+    }
+
+}
