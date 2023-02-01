eb24ca5c0d1dab0a42ecbbfc96bf0f2a789b4ba5
Ivanildo Junior <ivanildo@outlook.com>
2023-02-01 17:16:53 -0300

    creating practice 24 syncronize with not syncronized block

---
+++ src/main/java/pratica24/ThreadAR.java
@@ -0,0 +1,24 @@
+package pratica24;
+
+public class ThreadAR implements Runnable {
+
+    /*
+    Ao criar o bloco syncronized precisa informar qual o objeto esta sendo sincronizado
+     */
+    @Override
+    public void run() {
+
+        int threadCont;
+
+        synchronized (this) {
+            App.contador++;
+            threadCont = App.contador * 2;
+        }
+
+        double pot = threadCont * 50;
+        double raiz = Math.sqrt(pot);
+        System.out.println(raiz);
+
+    }
+
+}
